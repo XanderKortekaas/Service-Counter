941e1c31803022aeb938e61d7732e3241ebf6e19
Sidney <900224@student.glu.nl>
2025-11-03 14:26:35 +0100

    Button Fix
    Button Fix On Index (Please Test)

--- app/_styleSheet.js
+++ app/_styleSheet.js
@@ -1,25 +1,27 @@
 import { StyleSheet } from "react-native";
 import Colors from './_color';
 
-
 const styles = StyleSheet.create({
-
   "style": {
     "backgroundColor": Colors.GRAY_800,
-    "flex": 1, 
-    "justifyContent": "center", 
-    "alignItems": "center" 
+    "flex": 1,
+    "justifyContent": "center",
+    "alignItems": "center"
   },
-  "app":{
+  "app": {
   },
-  "app_header": {
+  "app_header_container": {
     "alignItems": "center",
-    "fontSize": 48, 
-    "color": Colors.BLUE_700 , 
+    "marginBottom": 30,
+    "paddingTop": 10
   },
-  "app_text":{
-    "fontSize":24,
-    "color": Colors.OLIVE_500, 
+  "app_header_text": {
+    "fontSize": 48,
+    "color": Colors.BLUE_700,
+  },
+  "app_text": {
+    "fontSize": 24,
+    "color": Colors.OLIVE_500,
     "marginBottom": 25,
   },
   "app-container": {
@@ -28,58 +30,58 @@ const styles = StyleSheet.create({
     "alignItems": "center"
   },
   "counter_container": {
-    "flex": 1 ,
+    "flex": 1,
     "alignItems": "center",
     "height": '100%',
   },
   "counter_viewer": {
-    "flexDirection": "Row",
+    "flexDirection": "row",
     "flexWrap": "wrap",
     "justifyContent": "center",
     "marginTop": 40,
     "marginBottom": 20,
-    "gap": 20, 
+    "gap": 20,
   },
   "counter_section": {
-    "flexDirection":'row',
+    "flexDirection": 'row',
     "alignItems": 'center',
-    "justifyContent":'space-between',
+    "justifyContent": 'space-between',
   },
-  "counter_section_h1":{
+  "counter_section_h1": {
     "flex": 1,
-    "color": Colors.GREEN_200, 
+    "color": Colors.GREEN_200,
     "fontSize": 40,
   },
-  "department_section":{
-    "flexDirection":'row',
+  "department_section": {
+    "flexDirection": 'row',
     "alignItems": 'center',
-    "justifyContent":'space-between',
+    "justifyContent": 'space-between',
     "padding": 20,
   },
-  "department":{
+  "department": {
     "color": Colors.BLUE_700,
     "fontSize": 36,
   },
-  "button_group":{
+  "button_group": {
     "flexDirection": "row",
-  }, 
+  },
   "button": {
     "marginTop": 10,
     "marginRight": 25,
     "marginLeft": 25,
-    "alignItems":"center",
+    "alignItems": "center",
   },
   "button_layout": {
     "backgroundColor": Colors.GREEN_500,
     "borderRadius": 5,
     "minWidth": 75,
-    "maxWidth": 150,  
+    "maxWidth": 150,
   },
   "button_text": {
     "fontSize": 25,
     "color": Colors.GRAY_900,
   },
-   "button_titel_text": {
+  "button_titel_text": {
     "fontSize": 25,
     "color": Colors.GREEN_500,
   },
@@ -87,7 +89,7 @@ const styles = StyleSheet.create({
     "height": 100,
     "width": 100
   },
-  "modal_content":{
+  "modal_content": {
     "alignItems": "center",
   },
   "modal_button": {
@@ -95,16 +97,17 @@ const styles = StyleSheet.create({
     "borderRadius": 5,
     "marginBottom": 50,
     "marginTop": 50,
+    "paddingHorizontal": 20,
+    "paddingVertical": 10,
   },
-  "modal_button_text":{
+  "modal_button_text": {
     "fontSize": 20,
     "color": Colors.GRAY_800,
   },
-  "modal_text":{
+  "modal_text": {
     "fontSize": 20,
     "color": Colors.GRAY_200,
   },
 });
 
-export default styles
-
+export default styles;
--- app/index.tsx
+++ app/index.tsx
@@ -1,25 +1,28 @@
 import { Link } from 'expo-router';
 import React from 'react';
-import { ScrollView, Text, TextStyle, TouchableOpacity, View, ViewStyle } from 'react-native';
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import DepartmentCounter from './DepartmentCounter';
 import styles from './_styleSheet';
 
 export default function Index() {
   return (
-    <View style={[styles.style as ViewStyle, { flex: 1 }]}>
+    <View style={[styles.style, { flex: 1 }]}>
       <ScrollView contentContainerStyle={{ padding: 20 }}>
-        <View style={styles.app_header as ViewStyle}>
-          <Text style={styles.app_header as TextStyle}>
+        {/* Vroeger: styles.app_header. Nu: styles.app_header_container */}
+        <View style={styles.app_header_container}>
+          {/* Vroeger: styles.app_header. Nu: styles.app_header_text */}
+          <Text style={styles.app_header_text}>
             Welcome to the Chistelijke Hogeschool Ede
           </Text>
-            <TouchableOpacity style={[styles.modal_button as ViewStyle]}>
-              <Link href="/adminPanel" asChild>
-                <Text style={styles.button_text as TextStyle}>Admin Panel</Text>
-              </Link>
+          
+          <Link href="/adminPanel" asChild>
+            <TouchableOpacity style={styles.modal_button}>
+              <Text style={styles.button_text}>Admin Panel</Text>
             </TouchableOpacity>
+          </Link>
         </View>
 
-        <View style={styles['counter_container'] as ViewStyle}>
+        <View style={styles.counter_container}>
           <DepartmentCounter departmentName="IT" />
           <DepartmentCounter departmentName="Finance" />
           <DepartmentCounter departmentName="Internal Affairs" />
